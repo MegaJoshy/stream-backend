3399f2b4bfea01b71e78c3ae2f0505d6e963db14
Joshua Pemberton <joshp2865@gmail.com>
2020-12-20 19:38:25 +0000

    magic

--- src/api/np.js
+++ src/api/np.js
@@ -7,8 +7,8 @@ const Vibrant = require('node-vibrant')
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const { data } = await fetch('https://api.livida.net/api/radio/').then(res => res.json())
-  const colours = await Vibrant.from(data.song.art).maxColorCount(2).getPalette()
+  const { album } = await fetch('https://api.livida.net/api/radio/').then(res => res.json())
+  const colours = await Vibrant.from(album.art).maxColorCount(2).getPalette()
 
   registerFont(`${process.cwd()}/assets/OpenSans-Bold.ttf`, { family: 'OpenSans Bold' })
   registerFont(`${process.cwd()}/assets/OpenSans-Regular.ttf`, { family: 'OpenSans' })
